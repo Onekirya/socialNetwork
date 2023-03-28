127d592b6f2e75d073cd028b5d212f80abe152e4
Kirill <98609018+Onekirya@users.noreply.github.com>
2023-03-28 21:04:10 +0300

    redux-form

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -5,16 +5,18 @@ import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsePage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer,
 });
 
-let store = legacy_createStore (reducers, applyMiddleware(thunkMiddleware));
+let store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
